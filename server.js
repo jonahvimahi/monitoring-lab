65ee79a579df593a852bb0f2fce74d6bf3616c0b
jonahvimahi <jonahvimahi@gmail.com>
2021-08-31 15:31:19 -0600

    changed token

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require('path')
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
-    accessToken: "359cb8a0be214ca2aeb1cd8e5f61050c",
+    accessToken: "1fab05d3ebcf4d349c0b0a56dfd21394",
     captureUncaught: true,
     captureUnhandledRejections: true
 })
