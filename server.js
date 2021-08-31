5375225d9b641e888e5114951cc4b678462ec3ef
jonahvimahi <jonahvimahi@gmail.com>
2021-08-31 13:55:23 -0600

    boiler plate

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`we are up and running on ${port}`))
